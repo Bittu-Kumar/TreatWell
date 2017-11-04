c60161e8ec62649ec9449d16ec7b362d15de3e44
Bittu Kumar <bittu.dakshana15@gmail.com>
2017-11-05 01:02:26 +0530

    created HomeActivity and SearchFragment

---
+++ app/src/main/java/com/example/bittukumar/treatwell/Fragments/SearchFragment.java
@@ -0,0 +1,28 @@
+package com.example.bittukumar.treatwell.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bittukumar.treatwell.R;
+
+/**
+ * Created by Bittu Kumar on 05-11-2017.
+ */
+
+public class SearchFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
